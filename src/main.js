f5f2517dd619eeff5d22c37278cd9a11cef25c93
Oran Shadian <oshadian@ucsc.edu>
2021-04-12 20:29:36 -0700

    Set up background

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    scene: [Menu, Play],
 }
 
 let game = new Phaser.Game(config);
-test
 
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
