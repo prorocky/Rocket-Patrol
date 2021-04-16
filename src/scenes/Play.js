e010ef94c6481453b21a71bd048674d6506f2a6f
oshadian <oshadian@ucsc.edu>
2021-04-15 17:01:53 -0700

    edited name of Rocket.png to rocket.png

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,7 +6,7 @@ class Play extends Phaser.Scene {
     preload() {
         // load images/sprites
         this.load.image('starfield', 'assets/starfield.png');
-        this.load.image('rocket', 'assets/Rocket.png');
+        this.load.image('rocket', 'assets/rocket.png');
         this.load.image('spaceship', 'assets/spaceship.png');
         this.load.spritesheet('explosion', 'assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     
